15c8313cb19c665adb58925c7a8290d472cfcd98
Stefan Begerad <dialog@swingbe.de>
2022-01-05 12:35:23 -0500

    test: added unit test

---
+++ src/test/java/de/swingbe/ifleet/TelegramParserImplTest.java
@@ -0,0 +1,22 @@
+package de.swingbe.ifleet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Test telegram parsing.
+ * <p>
+ * Note that this class is package-private, so that the client can not use it.
+ */
+class TelegramParserImplTest {
+
+    private final TelegramParserImpl parser = new TelegramParserImpl();
+
+    @Test
+    void padLeft() {
+        assertThat(parser.parse("TODO"))
+                .isEqualTo("to be done");
+    }
+
+}
